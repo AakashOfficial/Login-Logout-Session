0e0ad2cdc5779f2c7832e104d92a1bf461d2f269
AakashOfficial <aakashtyagi1996@outlook.com>
2020-01-30 15:20:47 +0530

    Project Created

---
+++ Login Logout Session/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Login_Logout_Session.Startup))]
+namespace Login_Logout_Session
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
