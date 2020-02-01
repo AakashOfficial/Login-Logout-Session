a9535b861f5c145d8babd0971e49a78bfffb4368
AakashOfficial <aakashtyagi1996@outlook.com>
2020-02-01 08:22:31 +0530

    Add Phone Number Perform

--- Login Logout Session/Controllers/ManageController.cs
+++ Login Logout Session/Controllers/ManageController.cs
@@ -105,6 +105,30 @@ namespace Login_Logout_Session.Controllers
         {
             return View();
         }
+
+        //
+        // POST: /Manage/AddPhoneNumber
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddPhoneNumber(AddPhoneNumberViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // Generate the token and send it
+            var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), model.Number);
+            if (UserManager.SmsService != null)
+            {
+                var message = new IdentityMessage
+                {
+                    Destination = model.Number,
+                    Body = "Your security code is: " + code
+                };
+                await UserManager.SmsService.SendAsync(message);
+            }
+            return RedirectToAction("VerifyPhoneNumber", new { PhoneNumber = model.Number });
+        }
         #region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
